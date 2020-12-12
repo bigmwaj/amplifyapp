4d147431d9417b88c433abdf3731b135dee895c7
Alain Joel Mouafo Wakem <alainmouafo@gmail.com>
2020-12-12 11:05:08 -0500

    Be proud

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import bams from './bams.jpg';
 import './App.css';
 
 function App() {
@@ -7,7 +8,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Bigmwaj wish hello from AWS</h1>
+        <h1>Bigmwaj wishes Happy Cameroon Independance Day from AWS</h1>
+        <p>
+        	<img src={bams} className="bams-proud" alt="Bams proud" />
+        	<br>Be proud of who we are ...
+        <p>
       </header>
     </div>
   );
